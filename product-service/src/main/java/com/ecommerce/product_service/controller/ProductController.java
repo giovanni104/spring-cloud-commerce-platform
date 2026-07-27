3a37d04dedf30a8c365be84d547603d9dd1722a1
Giovanny Hernandez Arbelaez <giovanni104@hotmail.com>
2026-07-27 09:36:14 -0500

    fuentes de proyectos

---
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -0,0 +1,64 @@
+package com.ecommerce.product_service.controller;
+
+import com.ecommerce.product_service.dto.ProductRequestDTO;
+import com.ecommerce.product_service.dto.ProductResponseDTO;
+import com.ecommerce.product_service.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponseDTO createProduct(@RequestBody @Valid ProductRequestDTO productRequestDTO){
+        return productService.createProduct(productRequestDTO);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponseDTO> getAllProducts() {
+        return productService.getAllsProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDTO getProductById(@PathVariable String id){
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id){
+        productService.deleteProduct(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductResponseDTO updateProduct(@PathVariable String id,
+                                            @RequestBody @Valid ProductRequestDTO productRequestDTO){
+
+        return productService.updateProduct(id, productRequestDTO);
+    }
+
+    @GetMapping("/test-fail")
+    public void testFail() {
+        throw new RuntimeException("¡Boom! La base de datos explotó (simulacro)");
+    }
+
+
+
+
+
+
+
+
+
+}
